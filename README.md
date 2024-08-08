9f7064abf9b2b2f2fb1011f4ea0ee044faf1a0c1
Srivatsav Erramilli <srivatsaverramilli@gmail.com>
2024-08-07 18:54:37 -0700

    chore: Initialize ERC-20 token project with basic files and dependencies

---
+++ README.md
@@ -0,0 +1,3 @@
+# ERC-20 Token
+
+This project creates a new ERC-20 token.
---
+++ contracts/MyToken.sol
@@ -0,0 +1,5 @@
+/** This file creates a new token using the ERC-20 token library from openzeppelin
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
